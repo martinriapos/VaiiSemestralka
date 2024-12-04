dc398ed6142e5c788c13c2ee029943507d4c6b0c
mrmar <mrmartinriapos@gmail.com>
2024-12-04 13:44:27 +0100

    Zakladna uprava vzhladu strank, pridanie noveho view registration uprava controleru v HomeController

---
+++ App/Views/Home/registration.view.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Registracia</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- Úvod stránky -->
+<section class="bg-primary text-white text-center py-5">
+    <div class="container">
+        <h1>Zaregistrujte sa.</h1>
+        <p class="podnadpis">Zaragistrujte sa a pridajte sa ku skvelej komunite ľuďí.</p>
+    </div>
+</section>
+
+<!-- Registracia pomocou forms -->
+<div class="container d-flex justify-content-center align-items-center">
+    <div class="row">
+        <div class="col">
+            <!-- Vrch text uvod -->
+            <h2 class="text-center">Registrácia</h2>
+            <form class="custom-registration-form">
+                <!-- 1.label/header -->
+                <div class="form-group">
+                    <label for="username">Používateľské meno</label>
+                    <input type="text" id="username" class="form-control" required>
+                </div>
+                <!-- 2.label/header -->
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" id="email" class="form-control" required>
+                </div>
+                <!-- 3.label/header -->
+                <div class="form-group">
+                    <label for="password">Heslo</label>
+                    <input type="password" id="password" class="form-control" required>
+                </div>
+                <!-- 4.label/header -->
+                <div class="form-group">
+                    <label for="confirm-password">Potvrdiť heslo</label>
+                    <input type="password" id="confirm-password" class="form-control" required>
+                </div>
+                <!-- Button na registraciu -->
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary">Zaregistrovať sa</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Spodok stranky -->
+<footer class="footer">
+    <div class="container text-center">
+        <p class="upper">&copy; 2024 Posilkaris. Všetky práva vyhradené.</p>
+        <p class="lower">
+            Kontakt: <a href="mailto:info@posilkaris.sk">info@posilkarisfirma.sk</a> | Telefón: +421 123 456 789
+        </p>
+    </div>
+</footer>
+
+</body>
+</html>
