e2da3262441a698b2e311cb2fefd109f7d1c3394
mrmar <mrmartinriapos@gmail.com>
2024-12-04 15:10:10 +0100

    pridanie Model DB.

---
+++ App/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+class User extends \App\Core\Model
+{
+    protected ?int $id = null;
+
+    protected ?string $name;
+    protected ?string $username;
+    protected ?string $password;
+    protected ?string $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+}
