3669aa569bd2c3d85415659b55d4811e05af5225
mrmar <mrmartinriapos@gmail.com>
2025-01-13 16:00:50 +0100

    vytvaranie CRUD operaciu nad modelom Products delete a create

---
+++ App/Views/Admin/addproducts.view.php
@@ -0,0 +1,40 @@
+<?php
+/** @var Array $data */
+/** @var \App\Core\LinkGenerator $link */
+?>
+
+<div class="container-fluid" style="min-height: 100vh">
+    <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card card-signin my-5">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Pridanie produktu</h5>
+                    <div class="text-center text-danger mb-3">
+                        <?= @$data['message'] ?>
+                    </div>
+                    <form class="form-signin" method="post" action="<?= $link->url("auth.addproduct") ?>">
+                        <div class="form-label-group mb-3">
+                            <input name="productname" type="text" id="productname" class="form-control" placeholder="Product name" required autofocus>
+                        </div>
+                        <div class="form-label-group mb-3">
+                            <input name="name" type="text" id="name" class="form-control" placeholder="Name" required>
+                        </div>
+                        <div class="form-label-group mb-3">
+                            <input name="price" type="number" step="0.01" min="0" id="price" class="form-control" placeholder="Price" required>
+                        </div>
+                        <div class="form-label-group mb-3">
+                            <input name="stock" type="number" step="1" min="0" id="stock" class="form-control" placeholder="Stock" required>
+                        </div>
+                        <div class="form-label-group mb-3">
+                            <input name="text" type="password" id="text" class="form-control" placeholder="Text" required>
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-primary" type="submit" name="submit">Pridať produkt
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
